4b4b7c3d58bd58ad1573439f1f00d879f6495693
Im-Stivik <mogil.sharon@gmail.com>
2023-11-15 15:49:48 +0200

    fixed the database creation

--- downloadData.py
+++ downloadData.py
@@ -1,9 +1,11 @@
 import requests
 import sqlite3
+import os
 
 url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
 
 url_modifier = "?&startdate=01/01/2000&enddate=08/23/2002&dateregion=tcg_date&sort=new"
+#url_modifier = "?"
 
 cards = requests.get(url + url_modifier)
 
@@ -13,9 +15,17 @@ connection = sqlite3.connect("database.sqlite")
 
 cursor = connection.cursor()
 
-
-cursor.execute("""CREATE TABLE IF NOT EXISTS card (name TEXT, type TEXT, frameType TEXT, 
-                desc TEXT, atk NUMBER, def NUMBER, level NUMBER, race TEXT,
+cursor.execute("DROP TABLE IF EXISTS card")
+cursor.execute("""CREATE TABLE IF NOT EXISTS card (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                name TEXT, 
+                type TEXT, 
+                frameType TEXT, 
+                desc TEXT, 
+                atk INTEGER, 
+                def INTEGER, 
+                level INTEGER, 
+                race TEXT,
                 attribute TEXT)""")
 
 for card in cards_json:
@@ -31,15 +41,24 @@ for card in cards_json:
     if card.get('attribute') is None:
         card['attribute'] = ""
 
-    cursor.execute("INSERT INTO card VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                   [card['name'], card['type'], card['frameType'], card['desc'],
-                    card['atk'], card['def'], card['level'], card['race'],
-                    card['attribute']])
+    cursor.execute("INSERT INTO card VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                   [card['id'], card['name'], card['type'], card['frameType'],
+                    card['desc'], card['atk'], card['def'], card['level'],
+                    card['race'], card['attribute']])
 
+    index = 0
     for image in card['card_images']:
+        filename = "./res/images/cards/faces/" + card['name'] + '/' + str(index) + ".jpg"
+
+        if (os.path.exists(filename)):
+            continue
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
         url = image['image_url']
         response = requests.get(url)
-        with open("res/images/cards/faces/" + str(image['id']) +".jpg", "wb") as f:
+        with open(filename, "wb") as f:
             f.write(response.content)
+        index += 1
 
     print('added ' + card['name'] + ' to database')
+
+connection.commit()
